65dd8e60a51ce2b8bd9241704ce2f94807f85a02
axionos <axionoso@gmail.com>
2019-05-20 23:12:25 -0400

    Done.

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const monsterURL = "http://localhost:3000/monsters"
 const formContainer = document.querySelector("#create-monster");
 const monsterContainer = document.querySelector("#monster-container");
 
-fetch(`${monsterURL}/?_limit=50&_page=1`)
+let page = 1
+fetch(`${monsterURL}/?_limit=50&_page=${page}`)
   .then(function(res){
     return res.json();
   })
   .then(function(monsters){
-
+    console.log(monsters)
     // RENDERING 50 MONSTERS
     for(let monster of monsters){
+      // monsterContainer.innerHTML = ""
       monsterContainer.innerHTML += `
         <div>
           <h2>${monster.name}</h2>
@@ -37,13 +39,14 @@ fetch(`${monsterURL}/?_limit=50&_page=1`)
 
   }); // ENDING GET FETCH
 
+
+
   const monsterForm = document.querySelector("#monsterForm");
   const monsterName = document.querySelector("#monsterName");
   const monsterAge = document.querySelector("#monsterAge");
   const monsterDesc = document.querySelector("#monsterDesc");
 
-  monsterForm.addEventListener("submit", function(e){
-    e.preventDefault();
+  monsterForm.addEventListener("submit", function(){
 
     fetch(monsterURL, {
       method: "POST",
@@ -52,11 +55,9 @@ fetch(`${monsterURL}/?_limit=50&_page=1`)
         "Accept": "application/json"
       },
       body: JSON.stringify({
-        monster: {
           name: monsterName.value,
           age: monsterAge.value,
           description: monsterDesc.value
-        }
       })
     })
     .then(function(res){
@@ -64,14 +65,80 @@ fetch(`${monsterURL}/?_limit=50&_page=1`)
     })
     .then(function(json){
       console.log(json);
-      // debusgger
 
       monsterContainer.innerHTML += `
         <div>
-          <h2>${monster.name.value}</h2>
-          <h4>Age: ${monster.age.value}</h4>
-          <p>Bio: ${monster.description.value}</p>
+          <h2>${json.name}</h2>
+          <h4>Age: ${json.age}</h4>
+          <p>Bio: ${json.description}</p>
         </div>
       `
-    });
+    }); // ENDING POST FETCH
+
   }); // ENDING MONSTER FORM
+
+
+
+  // NEXT BUTTON
+  const nextBtn = document.querySelector("#forward");
+
+  nextBtn.addEventListener("click", function(){
+    page++;
+    console.log(page);
+    fetch(`http://localhost:3000/monsters/?_limit=50&_page=${page}`)
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(monsters){
+      console.log(monsters)
+
+      monsterContainer.innerHTML = ""
+
+      for(let monster of monsters){
+        monsterContainer.innerHTML += `
+        <div>
+        <h2>${monster.name}</h2>
+        <h4>Age: ${monster.age}</h4>
+        <p>Bio: ${monster.description}</p>
+        </div>
+        `
+      }
+      document.body.scrollTop = 0; // for Safari
+      document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE, Opera
+
+    }) // END OF GET FETCH
+  }); // END OF NEXT BUTTON EVENT LISTENER
+
+
+
+
+  // PREVIOUS BUTTON
+
+  const preBtn = document.querySelector("#back");
+
+  preBtn.addEventListener("click", function(){
+    page--;
+    console.log(page);
+    fetch(`http://localhost:3000/monsters/?_limit=50&_page=${page}`)
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(monsters){
+      console.log(monsters)
+
+      monsterContainer.innerHTML = ""
+
+      for(let monster of monsters){
+        monsterContainer.innerHTML += `
+        <div>
+        <h2>${monster.name}</h2>
+        <h4>Age: ${monster.age}</h4>
+        <p>Bio: ${monster.description}</p>
+        </div>
+        `
+      }
+      document.body.scrollTop = 0; // for Safari
+      document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE, Opera
+
+    }) // END OF GET FETCH
+  }); // END OF PREVIOUS BUTTON EVENT LISTENER
